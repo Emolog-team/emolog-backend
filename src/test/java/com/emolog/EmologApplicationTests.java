f7ef35f7c1cbfadabddd1574294cc0ca7c024575
DDonghyeo <gsu9045000@gmail.com>
2025-09-21 23:21:15 +0900

    📝 docs : 프로젝트 뼈대 생성

---
+++ src/test/java/com/emolog/EmologApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmologApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
